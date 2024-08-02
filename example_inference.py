cfc24bc40ac7966616a5e2ad177b39eb678ef15e
RaviRaj Ranadive <ravirajranadive@PIX-LAP-119.local>
2024-08-02 12:35:00 +0530

    final training pipeline.

--- example_inference.py
+++ example_inference.py
@@ -1,48 +1,47 @@
 from skimage import io
 import torch, os
+import numpy as np
 from PIL import Image
-from briarmbg import BriaRMBG
-from utilities import preprocess_image, postprocess_image
+import torch.nn.functional as F
 from huggingface_hub import hf_hub_download
 from skimage.color import rgba2rgb, gray2rgb
+from torchvision.transforms.functional import normalize
 
 
-def example_inference(image_path, net):
-    im_path = image_path
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    net.to(device)  
-    # prepare input
-    model_input_size = [1024,1024]
-    orig_im = io.imread(im_path)
-    # Convert to RGB if image is grayscale
-    if len(orig_im.shape) == 2 or orig_im.shape[2] == 1:
-        orig_im = gray2rgb(orig_im)
-
-    # Convert to RGB if image is RGBA
-    if orig_im.shape[2] == 4:
-        orig_im = rgba2rgb(orig_im)
+def resize_image(image):
+    image = image.convert('RGB')
+    model_input_size = (1024, 1024)
+    image = image.resize(model_input_size, Image.BILINEAR)
+    return image
 
 
-    orig_im_size = orig_im.shape[0:2]
-    image = preprocess_image(orig_im, model_input_size).to(device)
-    # print(image.shape)
-    # inference 
-    result=net(image)
-    del net
+def model_inference(image_path, net):
+    image = np.array(Image.open(image_path))
+    orig_image = Image.fromarray(image)
+    w,h = orig_im_size = orig_image.size
+    image = resize_image(orig_image)
+    im_np = np.array(image)
+    im_tensor = torch.tensor(im_np, dtype=torch.float32).permute(2,0,1)
+    im_tensor = torch.unsqueeze(im_tensor,0)
+    im_tensor = torch.divide(im_tensor,255.0)
+    im_tensor = normalize(im_tensor,[0.5,0.5,0.5],[1.0,1.0,1.0])
+    if torch.cuda.is_available():
+        im_tensor=im_tensor.cuda()
 
-    # print(result[0][0].shape)
+    result=net(im_tensor)
     # post process
-    result_image = postprocess_image(result[0][0], orig_im_size)
+    result = torch.squeeze(F.interpolate(result[0][0], size=(h,w), mode='bilinear') ,0)
+    ma = torch.max(result)
+    mi = torch.min(result)
+    result = (result-mi)/(ma-mi)    
+    # image to pil
+    im_array = (result*255).cpu().data.numpy().astype(np.uint8)
+    pil_im = Image.fromarray(np.squeeze(im_array))
+    # paste the mask on the original image
+    new_im = Image.new("RGBA", pil_im.size, (0,0,0,0))
+    new_im.paste(orig_image, mask=pil_im)
+    # new_orig_image = orig_image.convert('RGBA')
 
-    # save result
-    pil_im = Image.fromarray(result_image)
-    # no_bg_image = Image.new("RGBA", pil_im.size, (0,0,0,0))
-    # orig_image = Image.open(im_path)
-    # no_bg_image.paste(orig_image, mask=pil_im)
-    # no_bg_image.save("example_image_no_bg.png")
-    return pil_im
+    return new_im, pil_im
 
 
-# if __name__ == "__main__":
-#     example_inference()
--- rmbg_trainer.py
+++ rmbg_trainer.py
@@ -13,7 +13,8 @@ from skimage.color import rgba2rgb, gray2rgb
 from torch.utils.data import DataLoader
 from briarmbg import BriaRMBG
 from utilities import preprocess_image, postprocess_image
-from example_inference import example_inference
+from example_inference import model_inference
+from torch.utils.data import DataLoader, random_split
 
 
 
@@ -31,7 +32,7 @@ class CustomDataset(torch.utils.data.Dataset):
         im_tensor = F.interpolate(im_tensor.unsqueeze(0), size=model_input_size, mode='bilinear', align_corners=False)
         im_tensor = im_tensor.squeeze(0).type(torch.float32)
         image = im_tensor / 255.0
-        image = transforms.Normalize([0.5, 0.5, 0.5], [1.0, 1.0, 1.0])(image)
+        image = transforms.Normalize([0.485, 0.456, 0.456], [0.229, 0.224, 0.225])(image)
         return image
 
     def preprocess_mask(self, mask: np.ndarray, model_input_size: list) -> torch.Tensor:
@@ -64,6 +65,9 @@ class CustomDataset(torch.utils.data.Dataset):
         mask = self.preprocess_mask(orig_mask, self.model_input_size).to(self.device)
 
         return image, mask
+    
+
+
 
 def log_evaluation_images(model, image_paths, true_mask_paths, epoch):
     model.eval()
@@ -79,7 +83,7 @@ def log_evaluation_images(model, image_paths, true_mask_paths, epoch):
             img_np = np.array(img)
             mask_np = np.array(mask)
             # Get predicted mask from example_inference
-            pred_mask_np = example_inference(img_path, model)
+            pred_mask_np, removed_bkg = model_inference(img_path, model)
 
             image_np_list.append(img_np)
             mask_np_list.append(mask_np)
@@ -87,80 +91,118 @@ def log_evaluation_images(model, image_paths, true_mask_paths, epoch):
         model.train()
         return image_np_list, mask_np_list, pred_mask_np_list 
 
+
 def finetune_model(image_dir, mask_dir, model_path, epochs, batch_size, learning_rate):
-    # Initialize wandb
-    wandb.init(project="rmbg-finetuning-last-final")
+    wandb.init(project="Experiment-4-rmbg")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     # Dataset and dataloader
     dataset = CustomDataset(image_dir=image_dir, mask_dir=mask_dir, device=device)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-    # Load local weights(.pth)
+    train_size = int(0.9 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    
+    # Load local weights (.pth)
     model = BriaRMBG()
     if model_path:
-        print("Loading pretrained model. from :", model_path)
+        print("Loading pretrained model from:", model_path)
         model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-        
-
+    
     model = model.to(device)
     
     # Optimizer and loss
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-07, weight_decay=0)
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
     criterion = nn.BCEWithLogitsLoss()
-
+    
+    # Lists to store losses for wandb logging
+    train_losses = []
+    val_losses = []
+    
     # Train loop
-    model.train()
     for epoch in range(epochs):
+        model.train()
         epoch_loss = 0.0
-
+        
         # Wrap DataLoader with tqdm for progress bar
-        with tqdm(dataloader, desc=f'Epoch {epoch+1}/{epochs}', unit='batch') as dataloader_desc:
-            for images, masks in dataloader_desc:
+        with tqdm(train_loader, desc=f'Epoch {epoch+1}/{epochs}', unit='batch') as train_loader_desc:
+            for images, masks in train_loader_desc:
                 images = images.to(device)
                 masks = masks.to(device)
-
+                
                 optimizer.zero_grad()
                 outputs = model(images)[0][0]
-
+                
                 loss = criterion(outputs, masks)
                 loss.backward()
                 optimizer.step()
-
+                
                 epoch_loss += loss.item()
-
+                
                 # Update tqdm description with current loss
-                dataloader_desc.set_postfix(loss=loss.item())
-
-        avg_epoch_loss = epoch_loss / len(dataloader)   
-        # Log the loss to wandb
-        wandb.log({"epoch": epoch+1, "loss": avg_epoch_loss})
-
-        print(f'Epoch {epoch+1}/{epochs}, Avg Loss: {avg_epoch_loss}')
-
-        if (epoch) % 3 == 0:
-            image_np_list, mask_np_list, pred_mask_np_list = log_evaluation_images(model, eval_img_list, eval_masks_list, epoch)
-
-            # Log the images to wandb
-            wandb.log({
+                train_loader_desc.set_postfix(loss=loss.item())
+        
+        avg_epoch_loss = epoch_loss / len(train_loader)
+        train_losses.append(avg_epoch_loss)
+        # Log the training loss to wandb
+        wandb.log({"epoch": epoch+1,})
+        
+        print(f'Epoch {epoch+1}/{epochs}, Avg Train Loss: {avg_epoch_loss}')
+        
+        # Evaluation step
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for images, masks in val_loader:
+                images = images.to(device)
+                masks = masks.to(device)
+                
+                outputs = model(images)[0][0]
+                loss = criterion(outputs, masks)
+                val_loss += loss.item()
+        
+        avg_val_loss = val_loss / len(val_loader)
+        val_losses.append(avg_val_loss)
+        
+        # Log the validation loss to wandb
+        wandb.log({"epoch": epoch+1, })
+        
+        print(f'Epoch {epoch+1}/{epochs}, Avg Val Loss: {avg_val_loss}')
+        image_np_list, mask_np_list, pred_mask_np_list = log_evaluation_images(model, eval_img_list, eval_masks_list, epoch)
+        wandb.log({
             "epoch": epoch+1,
+            "Loss": {"train_loss":avg_epoch_loss, "validation_loss" : avg_val_loss},
             "Original Image": [wandb.Image(img) for img in image_np_list],
             "Bria Mask": [wandb.Image(bria_mask) for bria_mask in mask_np_list],
-            "Predicted Mask": [wandb.Image(pred_mask) for pred_mask in pred_mask_np_list]})
-            
-            torch.save(model.state_dict(), f'./weight_files_checkpoints/finetuned_model-ep{epoch+1}.pth')
+            "Predicted Mask": [wandb.Image(pred_mask) for pred_mask in pred_mask_np_list]
+        })
+        
+        torch.save(model.state_dict(), f'./weight_files_exp_4/finetuned_model-ep{epoch}.pth')
+        
+        lr_scheduler.step()
+        
+        for param_group in optimizer.param_groups:
+            current_lr = param_group['lr']
+        wandb.log({"epoch": epoch+1, "learning_rate": current_lr})
     
-    torch.save(model.state_dict(), './weight_files_checkpoints/final-finetuned-model.pth')
+    torch.save(model.state_dict(), './weight_files_exp_4/final-finetuned-model.pth')
     del model
 
 
 def main():
     # Load datasets
-    df_cleaned_train = pd.read_csv(".\\dataset-csv\\combined_train_data.csv")
-    evaluation_df = pd.read_csv(".\\dataset-csv\\evalution-data.csv")
+    df_cleaned_train = pd.read_csv("./dataset-csv/complete_dataset.csv")
 
-    images_paths_list = df_cleaned_train["images_path"]
-    masks_paths_list = df_cleaned_train["masks_path"]
+    # Shuffle the DataFrame rows
+    df_cleaned_train = df_cleaned_train.sample(frac=1, random_state=48).reset_index(drop=True)
+
+    evaluation_df = pd.read_csv("./dataset-csv/eval_data.csv")
+
+    images_paths_list = df_cleaned_train["image_path"]
+    masks_paths_list = df_cleaned_train["mask_path"]
 
     global eval_img_list, eval_masks_list
     eval_img_list = evaluation_df["images_path"]
@@ -169,10 +211,10 @@ def main():
     # Launch FineTuning
     finetune_model(image_dir=images_paths_list,
                    mask_dir=masks_paths_list,
-                   model_path=None,
-                   epochs=500,
+                   model_path="/media/pixis/pixis/Ravi_workspace/rembg-finetune/model.pth",
+                   epochs=150,
                    batch_size=8,
-                   learning_rate=1e-4)
+                   learning_rate=1e-3)
 
 if __name__ == "__main__":
     main()
